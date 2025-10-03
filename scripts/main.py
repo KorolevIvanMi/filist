13b36f894db57e0a6fde8f9d937d2bdf1ea598f9
ivankorolev <ikorolev368@gmail.com>
2025-10-03 19:01:52 +0300

    fix

--- scripts/main.py
+++ scripts/main.py
@@ -2,21 +2,15 @@ from kivy.config import Config
 Config.set('graphics', 'width', '1422')
 Config.set('graphics', 'height', '800')
 
-from kivy.uix.settings import text_type
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.lang import Builder
 import os
-from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import (
     NumericProperty, ReferenceListProperty, ObjectProperty
 )
 from FillistMainMenu import FillistMainMenu
 
-
-
 class FillistApp(App):
-    
     def build(self):
         
         kv_path = os.path.join(os.path.dirname(__file__), '..', 'design', 'fillist.kv')
