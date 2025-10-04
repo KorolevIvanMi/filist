8f39d1f785999ef0eaea2765dbe82486457a820c
ivankorolev <ikorolev368@gmail.com>
2025-10-04 19:22:32 +0300

    nothing chancheg

--- scripts/myLayout.py
+++ scripts/myLayout.py
@@ -1,15 +1,19 @@
-from kivy.uix.settings import text_type
-from kivy.app import App
-from kivy.uix.widget import Widget
+
 from kivy.lang import Builder
 import os
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import (
     NumericProperty, ReferenceListProperty, ObjectProperty
 )
+from kivy.uix.button import Button
+# from myDropDown import myDropDown
+
 class myLayout(FloatLayout):
     search_but = ObjectProperty(None)
     search_text = ObjectProperty(None)
+    
+
+
     def searchOnPress(self):
         text_to_find = self.search_text.text
         self.search_text.text = "Поиск..."
