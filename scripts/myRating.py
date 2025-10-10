4b9debb1aed8bc251d10309ccc4cd8a03cd1bc46
ivankorolev <ikorolev368@gmail.com>
2025-10-10 21:30:03 +0300

    add chanhing color and rating rady

--- scripts/myRating.py
+++ scripts/myRating.py
@@ -12,13 +12,21 @@ class CustomLayotForRating(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.width = 100
+        buttons = []
         self.orientation = 'horizontal'
         for i in range(5):
             btn = CustomButtonForRationg(text = str(i+1))
-            btn.bind(on_release = lambda btn: self.buttonIsDown(btn.text))
+            btn.bind(on_release = lambda btn: self.buttonIsDown(btn.text, buttons))
+            buttons.append(btn)
             self.add_widget(btn)
-            
-    def buttonIsDown(self, txt):
-        
+
+    def buttonIsDown(self, txt, buttons):    
         self.selected_rating = txt
-        
+        for button in buttons:
+            button.background_color = (136/255,136/255,136/255, 1)
+        for button in buttons:
+            if int(button.text) <= int(txt):
+                button.background_color = (1,204/255,0, 1)
+    
+
+    
