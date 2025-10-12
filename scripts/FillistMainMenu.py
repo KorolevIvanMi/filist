bc3c35b5345278534c0dae7b07583d4d3f263736
ivankorolev <ikorolev368@gmail.com>
2025-10-12 20:24:26 +0300

    fix

--- scripts/FillistMainMenu.py
+++ scripts/FillistMainMenu.py
@@ -2,5 +2,6 @@ from kivy.uix.widget import Widget
 from myLayout import myLayout
 
 
+
 class FillistMainMenu(Widget):
     pass
--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 from kivy.config import Config
 Config.set('graphics', 'width', '1920')
 Config.set('graphics', 'height', '1080')
+Config.set('graphics', 'resizable', False) 
 from kivy.app import App
 from kivy.lang import Builder
 import os
