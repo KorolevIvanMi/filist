f6c4f78757e0d8391b43a88ce23c6b5b9c62efe4
ivankorolev <ikorolev368@gmail.com>
2025-10-12 19:39:04 +0300

    add design

--- scripts/myScrolingMenu.py
+++ scripts/myScrolingMenu.py
@@ -1,14 +1,16 @@
+from kivy.uix.accordion import NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.recycleview import RecycleView
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-from kivy.properties import BooleanProperty, StringProperty
+from kivy.properties import BooleanProperty, StringProperty, NumericProperty
 
 class StatefulLabel(RecycleDataViewBehavior, BoxLayout):
     name = StringProperty()
     genre = StringProperty()
     status = StringProperty()
+    rating = NumericProperty()
     active = BooleanProperty()
     index = 0
 
@@ -17,6 +19,7 @@ class StatefulLabel(RecycleDataViewBehavior, BoxLayout):
         self.name = data.get('name', '')
         self.genre = data.get('genre', '')
         self.status = data.get('status', '')
+        self.rating = data.get('rating', '')
         super(StatefulLabel, self).refresh_view_attrs(rv, index, data)
 
 class RecycleGridLayout(GridLayout):
@@ -30,10 +33,10 @@ class RV(RecycleView):
         if data_list is None:
             # Данные по умолчанию, если список не передан
             data_list = [
-                {'name': 'Фильм 1', 'genre': 'Драма', 'status': 'Новый', 'active': False},
-                {'name': 'Фильм 2', 'genre': 'Комедия', 'status': 'Просмотрено', 'active': False},
-                {'name': 'Фильм 3', 'genre': 'Боевик', 'status': 'В процессе', 'active': False},
-                {'name': 'Фильм 4', 'genre': 'Фантастика', 'status': 'Новый', 'active': False},
-                {'name': 'Фильм 5', 'genre': 'Ужасы', 'status': 'Просмотрено', 'active': False},
+                {'name': 'Фильм 1', 'genre': 'Драма', 'status': 'Новый', 'rating': 4, 'active': False},
+                {'name': 'Фильм 2', 'genre': 'Комедия', 'status': 'Просмотрено', 'rating': 4, 'active': False},
+                {'name': 'Фильм 3', 'genre': 'Боевик', 'status': 'В процессе', 'rating': 4, 'active': False},
+                {'name': 'Фильм 4', 'genre': 'Фантастика', 'status': 'Новый', 'rating': 4, 'active': False},
+                {'name': 'Фильм 5', 'genre': 'Ужасы', 'status': 'Просмотрено', 'rating': 4, 'active': False},
             ]
         self.data = data_list
