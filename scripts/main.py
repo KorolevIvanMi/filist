b37d3545dabfce71621ae5f866b411a40de8cbd0
IvanKorolev <purpleguy15122005@gmail.com>
2025-10-22 15:40:52 +0300

    add one screen

--- scripts/main.py
+++ scripts/main.py
@@ -8,14 +8,21 @@ import os
 from kivy.properties import (
     NumericProperty, ReferenceListProperty, ObjectProperty
 )
+from kivy.uix.screenmanager import ScreenManager, Screen
 from FillistMainMenu import FillistMainMenu
+
+class mainScreen(Screen):
+     pass
 
 
 class FillistApp(App):
         def build(self):
+            sm = ScreenManager()
+            sm.add_widget(mainScreen(name = "mainScreen"))
             kv_path = os.path.join(os.path.dirname(__file__), '..', 'design', 'fillist.kv')
             Builder.load_file(kv_path)
             application = FillistMainMenu()
+            application.add_widget(sm)
             return application
 
 if __name__ == '__main__':
