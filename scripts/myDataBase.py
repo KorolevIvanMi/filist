1abe4773a4389adca70a37bfdc1376e307a83b8f
IvanKorolev <purpleguy15122005@gmail.com>
2025-10-29 18:17:47 +0300

    add add_film function

--- scripts/myDataBase.py
+++ scripts/myDataBase.py
@@ -87,6 +87,7 @@ class myDataBase:
                         WHERE filmlist.name = ?
                         ''', (film_name,))
             results = cur.fetchall()
+            
             films = []
             for row in results:
                 film_dict = {
@@ -100,6 +101,7 @@ class myDataBase:
                     'active': False,  
                 }
                 films.append(film_dict)
+            
             return films
         
     def get_all_films(self):
@@ -179,6 +181,78 @@ class myDataBase:
                 }
                 films.append(film_dict)
             return films
+         
+    def add_film_to_bd(self, film_name, film_genre, film_status, film_rating, film_discription):
+        with sq.connect(self.db_path) as con:
+            con.row_factory = sq.Row 
+            cur = con.cursor()
+            # проверка существования фильма
+            cur.execute('''
+                        SELECT filmlist.name,   genre.name as genre_name,  status.name as status_name, rating, filmlist.description, filmlist.film_id FROM filmlist
+                        JOIN genre ON filmlist.genre  = genre.genre_id
+                        JOIN status ON filmlist.status = status.status_id
+                        WHERE filmlist.name = ? AND genre.name = ?
+						AND status.name = ? AND rating = ?
+                        ''', (film_name, film_genre, film_status, film_rating,))
+            results = cur.fetchall()
+            films = []
+            for row in results:
+                film_dict = {
+                    'name': row['name'],
+                    'genre_id': row['genre_id']
+                }
+                films.append(film_dict)
+            if(films == []):
+                # добавление жанра в таблицу жанров и формирование genre_id
+                genre_id = -1
+                cur.execute('''SELECT * from genre where name = ?''', (film_genre,))
+                results = cur.fetchall()
+                films = []
+                for row in results:
+                    film_dict = {
+                        'name': row['name'],
+                        'genre_id': row['genre_id']
+                    }
+                    films.append(film_dict)
+                if (films == []):
+                    cur.execute('''INSERT INTO genre(name) VALUES (?)''', (film_genre,))
+                    genre_id = cur.lastrowid
+                else:
+                    genre_id = films[0]['genre_id']
+                # добавление статуса фильма и формирования status_id
+                status_id = -1
+                cur.execute('''SELECT * from genre where name = ?''', (film_status,))
+                results = cur.fetchall()
+                films = []
+                for row in results:
+                    film_dict = {
+                        'name': row['name'],
+                        'status_id': row['status_id']
+                    }
+                    films.append(film_dict)
+                status_id = films[0][status_id]
+                # создание rating_id
+                rating_id = film_rating
+                results = cur.fetchall()
+                films = []
+                for row in results:
+                    film_dict = {
+                        'name': row['name'],
+                        'genre_id': row['genre_id']
+                    }
+                    films.append(film_dict)
+                # добавление фильма в таблицу
+                cur.execute('''INSERT INTO filmlist(name, genre, status, rating, description) VALUES (?, ?, ?, ?, ?)''', 
+                            (film_name, genre_id, status_id, rating_id, film_discription, ))
+                print("Фильм добавлет в таблицу")
+                return 1
+            else:
+                print("Такой фильм уже существует, попробуйте заного")
+                return 0     
+            
+
+        
+        
 if __name__ == "__main__":
     db = myDataBase()
     db.db_init()
